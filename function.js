7250f08ab2d8a57d6cfda3debe5de73820a96f41
Psyxology <132628284+Psyxology@users.noreply.github.com>
2023-05-28 21:06:18 -0400

    ok

--- function.js
+++ function.js
@@ -1,4 +1,24 @@
-const randomContent = []
 
 
-function randomContentGenerator (); // will choose a link from an array
+
+
+resetBtn = document.getElementById('resetBtn');
+const buttonBox = document.getElementById('buttonBox');
+
+
+
+createButtonverse(10);
+
+function createButtonverse(buttons) {
+    for (let i = 0; i < buttons; i++) {
+        const randomButton = document.createElement('button');
+        randomButton.innerHTML = 'rcg';
+        buttonBox.appendChild(randomButton);
+          
+     
+      }
+      
+    }
+
+
+
